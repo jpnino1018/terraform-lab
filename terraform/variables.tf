0e2ac8518eec5b54a86d6e96446127ceaf2f61b8
Pablo Nino <juanpnino18@gmail.com>
2026-04-30 11:49:24 -0500

    Configure AWS provider with Terraform Cloud dynamic credentials

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "student_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
